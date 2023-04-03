4a46caf83165f1f84c23f4ecb24404693f69a1b6
aufgfua <augustofalcaoflach@gmail.com>
2023-04-03 14:57:59 -0300

    asdfaqwer

--- app/header/frontend/redirectMessages.h
+++ app/header/frontend/redirectMessages.h
@@ -2,6 +2,8 @@ void redirectMessage(int sock_src, int sock_dst)
 {
     cout << "Redirecting message from " << sock_src << " to " << sock_dst << endl;
 
+    cout << "FE>";
+
     char *data = read_all_bytes(sock_src, sizeof(packet));
 
     write_all_bytes(sock_dst, data, sizeof(packet));
