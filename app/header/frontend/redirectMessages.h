98f94fa54e2faa16352803068a7d4366f4d337b5
aufgfua <augustofalcaoflach@gmail.com>
2023-04-03 14:45:26 -0300

    qerwer

--- app/header/frontend/redirectMessages.h
+++ app/header/frontend/redirectMessages.h
@@ -1,5 +1,7 @@
 void redirectMessage(int sock_src, int sock_dst)
 {
+    cout << "Redirecting message from " << sock_src << " to " << sock_dst << endl;
+
     char *data = read_all_bytes(sock_src, sizeof(packet));
 
     write_all_bytes(sock_dst, data, sizeof(packet));
